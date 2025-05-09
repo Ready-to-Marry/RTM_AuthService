e4b73d39c35108e0349ec1d9b040d461ee4648d8
DanidanaCDY <lucy2315.ce@gmail.com>
2025-05-09 14:56:32 +0900

    chore: 프로젝트 이름 변경 및 테스트 코드 제거
    - settings.gradle에 rootProject.name='AuthService' 설정
    - 초기 테스트용 클래스(TestController, TestRepository, Users) 삭제

--- AuthService/src/main/java/ready_to_marry/authservice/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package ready_to_marry.authservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ready_to_marry.authservice.entity.user.Users;
-
-@Repository
-public interface TestRepository extends JpaRepository<Users, Long> {
-}
