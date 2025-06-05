0183eff40b590996c63003713c4c5ebaeeccfe83
DanidanaCDY <lucy2315.ce@gmail.com>
2025-06-05 11:33:05 +0900

    fix: SocialUserInfo JsonCreator 로직 추가하여 Naver 응답 ID 파싱 개선
    - 기존 @JsonProperty/@JsonAlias 매핑으로는 Naver “response.id” 필드를 처리하지 못하던 문제 수정
    - JsonNode 기반 @JsonCreator 생성자를 통해 Naver “response.id” 우선 추출 로직 추가
    - 카카오의 최상위 “id” 및 구글의 최상위 “sub” 필드도 그대로 파싱

--- AuthService/src/main/java/ready_to_marry/authservice/social/dto/external/SocialUserInfo.java
+++ AuthService/src/main/java/ready_to_marry/authservice/social/dto/external/SocialUserInfo.java
@@ -1,9 +1,8 @@
 package ready_to_marry.authservice.social.dto.external;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 /**
  * 소셜 인증 서버로부터 사용자 정보를 조회할 때 사용하는 DTO
@@ -12,12 +11,30 @@ import lombok.NoArgsConstructor;
  * 응답으로 제공되는 사용자 고유 ID를 매핑
  */
 @Getter
-@NoArgsConstructor
 public class SocialUserInfo {
-    // 소셜 사용자 고유 ID
-    // - Kakao/Naver: "id"
-    // - Google:      "sub"
-    @JsonProperty("id")
-    @JsonAlias("sub")
-    private String id;
+    private final String id;
+
+    /**
+     * @JsonCreator를 붙인 생성자에서 JsonNode를 통째로 받아와서,
+     * 각 소셜 로그인 응답 형태에 따라 id를 직접 추출
+     */
+    @JsonCreator
+    public SocialUserInfo(JsonNode rootNode) {
+        String extractedId = null;
+
+        // 1) 네이버 응답: 최상위가 아니라 "response" 객체 내부에 "id"가 있음
+        if (rootNode.has("response") && rootNode.get("response").has("id")) {
+            extractedId = rootNode.get("response").get("id").asText();
+
+            // 2) 카카오(또는 네이버 일부 버전) 응답: 최상위에 바로 "id" 필드가 있음
+        } else if (rootNode.has("id")) {
+            extractedId = rootNode.get("id").asText();
+
+            // 3) 구글 응답: 최상위에 "sub" 필드가 있음
+        } else if (rootNode.has("sub")) {
+            extractedId = rootNode.get("sub").asText();
+        }
+
+        this.id = extractedId;
+    }
 }
